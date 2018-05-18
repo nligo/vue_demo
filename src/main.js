7e5e494e095d4eb86e514f13b9d7119e2c3114b9
coffey <coffey@nligo.com>
2018-05-18 14:21:24 +0800

    修改一些东西

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 import router from './router.js'
 import 'es6-promise/auto'
 import Vuex from 'vuex'
-
 axios.defaults.baseURL = 'http://fy.cms.com/api' // 'https://prezujuan-api.kaoti100.com/api'
 Vue.prototype.$axios = axios
 new Vue({
